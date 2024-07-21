4eedbddaeb8d788dd3c9f06e7f53e89f053091b1
prabhakaran2304 <prabhavarul@gmail.com>
2024-07-21 10:17:29 +0530

    Update app.js

--- css/app.js
+++ css/app.js
@@ -8,27 +8,4 @@ function updateClock() {
   const time = `${hours}:${minutes}:${seconds}`;
   clockElement.textContent = time;
 }
-
 setInterval(updateClock, 1000);
-
-// Get the button element
-const changeColorButton = document.getElementById('changeColorButton');
-
-// Function to generate a random color
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-// Function to change the background color on button click
-function changeBackgroundColor() {
-  const newColor = getRandomColor();
-  document.body.style.backgroundColor = newColor;
-}
-
-// Add event listener to the button
-changeColorButton.addEventListener('click', changeBackgroundColor);
