f017665eb6655ddbe421a394eb6b5e26975dd5f7
prabhakaran2304 <prabhavarul@gmail.com>
2024-07-21 10:07:42 +0530

    Update app.js
    clock added

--- css/app.js
+++ css/app.js
@@ -1,3 +1,16 @@
+//clock
+function updateClock() {
+  const clockElement = document.getElementById("clock");
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const time = `${hours}:${minutes}:${seconds}`;
+  clockElement.textContent = time;
+}
+
+setInterval(updateClock, 1000);
+
 // Get the button element
 const changeColorButton = document.getElementById('changeColorButton');
 
